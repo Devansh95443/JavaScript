7e711efc97aa31fc8c8552322d52e24f3cb30cea
Devansh <devanshsrivastava@gmail.com>
2024-04-13 15:08:53 -0700

    new feature

---
+++ javascriptm/function.js
@@ -0,0 +1,16 @@
+const sum =((a,b)=>{
+
+    sum =a+b;
+    return sum;
+});
+const hello=(()=>{
+console.log ("my name is devansh");
+});
+function dec(a,b)
+{
+    sum =a+b;
+    return sum;
+}
+console.log(sum(4,5));
+console.log(hello());
+console.log(dec(4,6));
---
+++ javascriptm/string.js
@@ -0,0 +1,6 @@
+var name1="devansh";
+var a=name1.length;
+for (var i=0;i<a;i++)
+{
+    console .log(name1[i]);
+}
